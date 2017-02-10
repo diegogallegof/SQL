baeb9df002d7ce867efa8b9d7a71fde57066a8ae
Geoff Allix <geoffa@contentmaster.com>
2017-02-10 15:30:36 +0000

    Add files via upload

--- Labs/DAT215_4/Setup/Drop Indexes on FactInternetSales.sql
+++ Labs/DAT215_4/Setup/Drop Indexes on FactInternetSales.sql
@@ -1,25 +1,29 @@
 /************************************************************************
- * Step 2 - Drop existing indexes										*
+ * Drop existing indexes							*
  *																		*
- * Indexes and keys need to be removed to enable a clustered columnstore*
- * index.																*
- *																		*
+ * Indexes and keys need to be removed to enable a memory optimized     *																*
+ * table.																*
  *																		*
  ************************************************************************/
 
-/****** All indexes need to be removed before creating a clustered columnstore ******/
+-- All indexes need to be removed before creating a memory-optimized table
 SET STATISTICS TIME OFF
 GO
-USE [AdventureWorksDW]
+USE [AdventureWorksDW2012]
 GO
 
-DROP INDEX [IX_FactIneternetSales_ShipDateKey] ON [dbo].[FactInternetSales];
-DROP INDEX [IX_FactInternetSales_CurrencyKey] ON [dbo].[FactInternetSales];
-DROP INDEX [IX_FactInternetSales_CustomerKey] ON [dbo].[FactInternetSales];
-DROP INDEX [IX_FactInternetSales_DueDateKey] ON [dbo].[FactInternetSales];
-DROP INDEX [IX_FactInternetSales_OrderDateKey] ON [dbo].[FactInternetSales];
-DROP INDEX [IX_FactInternetSales_ProductKey] ON [dbo].[FactInternetSales];
-DROP INDEX [IX_FactInternetSales_PromotionKey] ON [dbo].[FactInternetSales];
+/****** Columnstore index needs to be dropped, also can only have 8 on the in-memory table ******/
+DROP INDEX [CCI_FactInternetSales] ON [dbo].[FactInternetSales] WITH ( ONLINE = OFF )
+DROP INDEX [PK_FactInternetSales_SalesOrderNumber_SalesOrderLineNumber] ON [dbo].[FactInternetSales]
+DROP INDEX [IX_FactInternetSales_DueDateKey] ON [dbo].[FactInternetSales]
+DROP INDEX [IX_FactInternetSales_OrderDateKey] ON [dbo].[FactInternetSales]
+DROP INDEX [IX_FactIneternetSales_ShipDateKey] ON [dbo].[FactInternetSales]
+DROP INDEX [IX_FactInternetSales_CurrencyKey] ON [dbo].[FactInternetSales]
+DROP INDEX [IX_FactInternetSales_CustomerKey] ON [dbo].[FactInternetSales]
+DROP INDEX [IX_FactInternetSales_ProductKey] ON [dbo].[FactInternetSales]
+DROP INDEX [IX_FactInternetSales_PromotionKey] ON [dbo].[FactInternetSales]
+
+
 GO
 
 /****** All foreign keys need to be removed ******/
@@ -31,6 +35,6 @@ ALTER TABLE [dbo].[FactInternetSales] DROP CONSTRAINT [FK_FactInternetSales_DimD
 ALTER TABLE [dbo].[FactInternetSales] DROP CONSTRAINT [FK_FactInternetSales_DimProduct];
 ALTER TABLE [dbo].[FactInternetSales] DROP CONSTRAINT [FK_FactInternetSales_DimPromotion];
 ALTER TABLE [dbo].[FactInternetSales] DROP CONSTRAINT [FK_FactInternetSales_DimSalesTerritory];
-ALTER TABLE [dbo].[FactInternetSalesReason] DROP CONSTRAINT [FK_FactInternetSalesReason_FactInternetSales]
-ALTER TABLE [dbo].[FactInternetSales] DROP CONSTRAINT [PK_FactInternetSales_SalesOrderNumber_SalesOrderLineNumber]
+ALTER TABLE [dbo].[FactInternetSalesReason] DROP CONSTRAINT [FK_FactInternetSalesReason_FactInternetSales];
+ALTER TABLE [dbo].[FactInternetSales] DROP CONSTRAINT [PK_FactInternetSales_SalesOrderNumber_SalesOrderLineNumber];
 GO
